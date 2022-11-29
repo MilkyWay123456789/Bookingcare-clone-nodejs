08db1576162f5d4cb05d8c12fbf619b4636d252c
htcreater <tienph.19th@sv.dla.edu.vn>
2022-11-29 21:15:55 +0700

    md api

--- src/controllers/doctorController.js
+++ src/controllers/doctorController.js
@@ -15,6 +15,48 @@ let getTopDoctorHome=async(req,res)=>{
     }
 }
 
+let getAllDoctors=async(req,res)=>{
+    try{
+        let doctors=await doctorService.getAllDoctorsService('All');
+        return res.status(200).json(doctors);
+    }catch(e){
+        console.log(e)
+        return res.status(200).json({
+            errCode:-1,
+            errMessage: "Error from server..."
+        })
+    }
+}
+
+let postInforDoctor=async(req,res)=>{
+    try{
+        let response=await doctorService.saveDetailInforDoctor(req.body);
+        return res.status(200).json(response);
+    }catch(e){
+        console.log(e)
+        return res.status(200).json({
+            errCode:-1,
+            errMessage: "Error from server..."
+        })
+    }
+}
+
+let getDetailDoctorsById=async(req,res)=>{
+    try{
+        let infor=await doctorService.getDetailDoctorsById(req.query.id);
+        return res.status(200).json(infor);
+    }catch(e){
+        console.log(e)
+        return res.status(200).json({
+            errCode:-1,
+            errMessage: "Error from server..."
+        })
+    }
+}
+
 module.exports={
-    getTopDoctorHome:getTopDoctorHome
+    getTopDoctorHome:getTopDoctorHome,
+    getAllDoctors:getAllDoctors,
+    postInforDoctor:postInforDoctor,
+    getDetailDoctorsById:getDetailDoctorsById,
 }
