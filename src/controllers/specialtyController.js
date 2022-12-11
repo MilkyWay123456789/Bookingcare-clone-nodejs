2915dd7aebf5aff650ac8ff6341b8a64a3b890ca
htcreater <tienph.19th@sv.dla.edu.vn>
2022-12-11 09:06:01 +0700

    load specialty

--- src/controllers/specialtyController.js
+++ src/controllers/specialtyController.js
@@ -13,6 +13,20 @@ let createNewSpecialty = async (req, res) => {
     }
 }
 
+let getAllSpecialty = async (req, res) => {
+    try {
+        let specialty = await specialtyService.getAllSpecialtyService();
+        return res.status(200).json(specialty);
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "Error from server..."
+        })
+    }
+}
+
 module.exports = {
-    createNewSpecialty: createNewSpecialty
+    createNewSpecialty: createNewSpecialty,
+    getAllSpecialty: getAllSpecialty
 }
--- src/services/specialtyService.js
+++ src/services/specialtyService.js
@@ -28,6 +28,29 @@ let createNewSpecialty = (data) => {
     })
 }
 
+let getAllSpecialtyService = () => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let data = await db.Specialty.findAll();
+            if (data && data.length > 0) {
+                data.map(item => {
+                    item.image = new Buffer(item.image, 'base64').toString('binary');
+                    return item;
+                })
+            }
+
+            resolve({
+                errCode: 0,
+                errMessage: 'OK',
+                data
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
 module.exports = {
-    createNewSpecialty: createNewSpecialty
+    createNewSpecialty: createNewSpecialty,
+    getAllSpecialtyService: getAllSpecialtyService
 }
