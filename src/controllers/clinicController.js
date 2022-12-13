727745fe161ddf31b62977e18e4678f906b55af0
htcreater <tienph.19th@sv.dla.edu.vn>
2022-12-13 20:37:35 +0700

    clinic

---
+++ src/controllers/clinicController.js
@@ -0,0 +1,46 @@
+import clinicService from "../services/clinicService"
+
+let createNewClinic = async (req, res) => {
+    try {
+        let infor = await clinicService.createNewClinic(req.body);
+        return res.status(200).json(infor);
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "Error from server..."
+        })
+    }
+}
+
+let getAllClinic = async (req, res) => {
+    try {
+        let specialty = await clinicService.getAllClinic();
+        return res.status(200).json(specialty);
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "Error from server..."
+        })
+    }
+}
+
+let getDetailClinicById = async (req, res) => {
+    try {
+        let specialty = await clinicService.getDetailClinicById(req.query.id);
+        return res.status(200).json(specialty);
+    } catch (e) {
+        console.log(e)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "Error from server..."
+        })
+    }
+}
+
+module.exports = {
+    createNewClinic: createNewClinic,
+    getAllClinic: getAllClinic,
+    getDetailClinicById: getDetailClinicById
+}
